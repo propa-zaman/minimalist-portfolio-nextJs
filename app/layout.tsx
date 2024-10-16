7d26a222f340f26adc5c0580435ffa09bc315a4b
Afra <propazaman12@gmail.com>
2024-10-16 17:35:43 +0600

    bug fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "./provider";
+import Image from "next/image";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -17,6 +18,11 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "Afra Zaman",
   description: "Mordern and Minimalist Portfolio",
+  icons: {
+    icon: "/favicon.svg", // Or use any other favicon file in the public directory
+    shortcut: "/favicon.svg",
+    apple: "/apple-touch-icon.png", // Optional, for Apple devices
+  },
 };
 
 export default function RootLayout({
